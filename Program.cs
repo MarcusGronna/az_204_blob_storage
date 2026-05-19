2a369bcc7959971a8c0c58979e3121d7cc412198
Marcus Grönnå <mgronna@gmail.com>
2026-05-19 13:41:15 +0200

    Add async Azure Blob Storage setup with credential config
    Imported Azure.Identity and configured DefaultAzureCredentialOptions to exclude environment and managed identity credentials. Introduced an async ProcessAsync method with placeholders for Blob Storage operations. Updated main program to await this method and prompt before exit.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using Azure.Identity;
+
+Console.WriteLine("Azure Blob Storage exercise\n");
+
+// Create a DefaultAzureCredentialOptions object to configure the DefaultAzureCredentials
+DefaultAzureCredentialOptions options = new()
+{
+    ExcludeEnvironmentCredential = true,
+    ExcludeManagedIdentityCredential = true
+};
+
+// Run the wzamples azynchronosly, wait for the results before proceeding
+await ProcessAsync();
+
+Console.WriteLine("\nPress enter to exit the sample application.");
+Console.ReadLine();
+
+async Task ProcessAsync()
+{
+    // CREATE A BLOB STORAGE CLIENT
+
+
+
+    // CREATE A CONTAINER
+
+
+
+    // CREATE A LOCAL FILE FOR UPLOAD TO BLOB STORAGE
+
+
+
+    // UPLOAD THE FILE TO BLOB STORAGE
+
+
+
+    // LIST BLOBS IN THE CONTAINER
+
+
+
+    // DOWNLOAD THE BLOB TO A LOCAL FILE
+}
